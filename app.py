5ff121fa1603f0f23d2f0eea0f1156f6831155de
mshvaiko <shvayko.maxim@gmail.com>
2021-04-21 01:31:12 +0300

    Add initial commit with basic impl
    Signed-off-by: mshvaiko <shvayko.maxim@gmail.com>

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+from screeninfo import get_monitors
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+monotor = get_monitors()[0]
+cords = {'top':0 , 'left': 0 , 'width': monotor.width, 'height': monotor.height }
+
+def gen_frames():  
+    while True:
+        with mss() as sct :
+            img = np.array(sct.grab(cords))
+            ret, buffer = cv2.imencode('.jpg', img)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host="192.168.31.38", debug=True)
---
+++ get_links.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+from screeninfo import get_monitors
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+''' Current implementation suports streaming with single monitor '''
+monotor = get_monitors()[0]
+cords = {'top':0 , 'left': 0 , 'width': monotor.width, 'height': monotor.height }
+
+def gen_frames():  
+    while True:
+        with mss() as sct :
+            img = np.array(sct.grab(cords))
+            ret, buffer = cv2.imencode('.jpg', img)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/screen_0')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    # change to your ip address
+    app.run(host="192.168.31.38", debug=True)
